bce2ad470b9cd2b30e2ec749c29b2681ed0386f7
aditya-0122 <aaaaadit2021@gmail.com>
2025-03-09 20:45:27 +0700

    Add project files.

---
+++ tpmodul4_103022300149/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using tpmodul4_103022300149;
+
+class Program
+{
+    static void Main()
+    {
+        KodePos kodePos = new KodePos();
+        Console.Write("Masukkan kelurahan: ");
+        string kelurahan = Console.ReadLine();
+
+        string kode = kodePos.GetKodePos(kelurahan);
+        if (kode != null)
+        {
+            Console.WriteLine($"Kode Pos {kelurahan}: {kode}");
+        }
+        else
+        {
+            Console.WriteLine("Kelurahan tidak ditemukan.");
+        }
+    }
+}
